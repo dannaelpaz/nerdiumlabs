27e8c44946d5bf4fac3eaa7432356fe4dfb8229d
Dannael Paz <dannaelpaz@gmail.com>
2025-05-18 09:03:09 -0300

    atualizacao

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 import { createClient } from '@supabase/supabase-js';
 import { SupabaseContext } from './lib/supabaseContext';
 
-const supabase = createClient('SUA_SUPABASE_URL', 'SUA_SUPABASE_ANON_KEY');
+const supabase = createClient('https://wgqjllmpecgakvmhvqye.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndncWpsbG1wZWNnYWt2bWh2cXllIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc0OTU4MTYsImV4cCI6MjA2MzA3MTgxNn0.-aCVHRXwfW7VrxVHgcYprbfD0Rq-rtdTy1Eqo8kAmew');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
